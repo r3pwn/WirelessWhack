213f26b72a9e43353b5c974320fa4b4c0feb99f4
Greg Willard (r3pwn) <wickett06@gmail.com>
2017-02-18 07:39:53 -0500

    add initial layout

--- app/src/main/java/com/r3pwn/wifiwhack/MainActivity.java
+++ app/src/main/java/com/r3pwn/wifiwhack/MainActivity.java
@@ -2,6 +2,9 @@ package com.r3pwn.wifiwhack;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        TextView sessiontv = (TextView) findViewById(R.id.sessionTV);
+        EditText sessionet = (EditText) findViewById(R.id.sessionET);
+        Button session = (Button) findViewById(R.id.sessionBtn);
     }
 }
